05d16cc501b52c3de5ee2e419d6bf29cec5a2e4b
ardevsrc <dbeast@tuta.io>
2022-03-29 23:37:24 -0400

    Added render view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const [markdown, setMarkdown] = useState('# markdown preview');
+
   return (
     <main>
       <section className="markdown">
-        <textarea className="input"></textarea>
+        <textarea 
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
       </section>
     </main>
   );
